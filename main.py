ee389e8ebc3f3cd854baacae1abd134080243510
buko106 <buko1062000@yahoo.co.jp>
2017-07-18 00:39:14 +0900

    ???

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ args = parser.parse_args()
 p=pyaudio.PyAudio()
 FORMAT=pyaudio.paInt16
 INPUT_CHANNELS  = 1
-CHUNK=2048
-RATE=22000
-
+CHUNK=4000
+RATE=44000
+ 
+# 
 input_stream=p.open( format = FORMAT,
 		     channels = INPUT_CHANNELS,
 		     rate = RATE,
@@ -35,6 +36,8 @@ class Car:
         self.gear = True
         self.handle = 50
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.neutral = args.freq
+
     # TCP Util
     def connect(self,host,port):
         print("connect to host =",host,"port =",port)
@@ -47,7 +50,7 @@ class Car:
             self.acc = param
             self.server.sendall(b"accelerator %d\n" % param )
     def decelerator( self, param ):
-        if 0 <= param <= 100:
+        if self.dec != param and 0 <= param <= 100:
             self.dec = param
             self.server.sendall(b"decelerator %d\n" % param )
     def wheel( self, param ):
@@ -67,9 +70,27 @@ class Car:
     def turn_left( self, diff ):
         self.wheel( self.handle - diff )
 
+    # mapping from voice to driving parameter
+    def get_wheel( self, pitch ):
+        diff = np.log(pitch/self.neutral) * 50
+        return min(100,max(0,int(diff+50)))
+
+    def get_accel_decel( self, volume ):
+        threshold = 3.
+        if( volume <= threshold ):
+            return 0,20
+        return min(50,int((volume-threshold)*4)),0
+
     # Called in while loop
-    def update( self, diff, volume ):
-        pass
+    def update( self, pitch, volume ):
+        w = self.get_wheel( pitch )
+        a,d = self.get_accel_decel( volume )
+        print(a,w)
+        self.wheel(w)
+        time.sleep(0.01)
+        self.accelerator(a)
+        time.sleep(0.01)
+        self.decelerator(d)
 
 ############################################################
 # 初期状態に戻す 計測開始
@@ -86,22 +107,20 @@ class Car:
 
 
 # initialize
-"""
 car = Car()
 wait = 0.05
 car.connect(args.host,args.port)
 time.sleep(wait)
 car.start()
 time.sleep(wait)
-car.accelerator(20)
+car.accelerator(0)
 time.sleep(wait)
 car.decelerator(0)
-"""
+time.sleep(wait)
 
 max_val = 2.**15
-min_freq = 50
+min_freq = 100
 max_freq = 500
-neutral  = args.freq
 
 # voice utils
 def fft_and_filter( data ):
@@ -110,9 +129,16 @@ def fft_and_filter( data ):
     freq     = np.fft.fftfreq(len(result), d=1.0/RATE)
     idx      = np.logical_and(min_freq<=freq,freq<=max_freq)
     return result[idx], freq[idx]
+
+def accel_low_pitch( data, freq ):
+    return data * np.exp(-freq/30)
+
 import matplotlib.pyplot as plt
 while input_stream.is_active():
     input = input_stream.read(CHUNK)
     input = np.frombuffer(input, dtype=np.int16)/max_val
     result,freq = fft_and_filter( input )
-    print(freq[np.argsort(result)[-3:]])
+    low_pitch = accel_low_pitch(result,freq)
+    pitch = freq[np.argmax(result)]
+    volume = np.average(np.sort(result)[-1:])
+    car.update( pitch, volume )
