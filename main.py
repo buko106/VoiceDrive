bbc3fcb76e792d4b962a8e4a828c88ae6f51dc59
buko106 <buko1062000@yahoo.co.jp>
2017-07-18 15:03:24 +0900

    temporal implementation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 import pyaudio
+import pygame
 import numpy as np
 import socket
 import time
@@ -17,9 +18,9 @@ args = parser.parse_args()
 p=pyaudio.PyAudio()
 FORMAT=pyaudio.paInt16
 INPUT_CHANNELS  = 1
-CHUNK=4000
-RATE=44000
- 
+RATE=20000
+CHUNK=RATE//10
+
 # 
 input_stream=p.open( format = FORMAT,
 		     channels = INPUT_CHANNELS,
@@ -37,11 +38,21 @@ class Car:
         self.handle = 50
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.neutral = args.freq
+        self.flag = 0
+        self.arrow = np.array([(20,0),(20,-150),(40,-150),(0,-200),(-40,-150),(-20,-150),(-20,0)],dtype=np.float)
+        self.acc_max = 25
 
     # TCP Util
     def connect(self,host,port):
         print("connect to host =",host,"port =",port)
         self.server.connect((host,port))
+    # GUI Util
+    def create_window(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((500,500))
+        self.screen.fill((0,0,0))
+        pygame.display.set_caption("VoiceDrive")
+        
     # Driving Utils
     def start(self):
         self.server.sendall(b"start\n")
@@ -50,7 +61,7 @@ class Car:
             self.acc = param
             self.server.sendall(b"accelerator %d\n" % param )
     def decelerator( self, param ):
-        if self.dec != param and 0 <= param <= 100:
+        if 0 <= param <= 100:
             self.dec = param
             self.server.sendall(b"decelerator %d\n" % param )
     def wheel( self, param ):
@@ -60,9 +71,9 @@ class Car:
     def shiftgear( self, param ):
         self.gear = param
         if self.gear:
-            self.server.sendall(b"shiftgear true\n" )
+            self.server.sendall(b"shiftgear 1\n" )
         else:
-            self.server.sendall(b"shiftgear false\n" )
+            self.server.sendall(b"shiftgear -1\n" )
     # Wrapper
     def turn_right( self, diff ):
         self.wheel( self.handle + diff )
@@ -78,20 +89,52 @@ class Car:
     def get_accel_decel( self, volume ):
         threshold = 3.
         if( volume <= threshold ):
-            return 0,20
-        return min(50,int((volume-threshold)*4)),0
-
+            return 0,50
+        return min(self.acc_max,int((volume-threshold)*2)),0
+    def update_window( self ):
+        self.screen.fill((0,0,0))
+        theta = np.radians(self.handle-50)*1.4
+        c, s = np.cos(theta), np.sin(theta)
+        R = np.array([[c, -s], [s, c]])
+        if self.gear :
+            arrow_polygon = (R.dot(self.arrow.T).T)*(0.1+0.9*self.acc/self.acc_max) + np.array([250,250])
+        else:
+            arrow_polygon = (R.dot(self.arrow.T).T)*(0.1+0.9*self.acc/self.acc_max)*np.array([[1,-1]]) + np.array([250,50])
+        pygame.draw.polygon(self.screen,(255,255,0),arrow_polygon)
+        pygame.display.update()
     # Called in while loop
     def update( self, pitch, volume ):
         w = self.get_wheel( pitch )
         a,d = self.get_accel_decel( volume )
-        print(a,w)
-        self.wheel(w)
-        time.sleep(0.01)
-        self.accelerator(a)
-        time.sleep(0.01)
-        self.decelerator(d)
 
+        some_key_pressed = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.shiftgear( not self.gear )
+                    print("gear =",self.gear)
+                    some_key_pressed = True
+                if event.key == pygame.K_ESCAPE:
+                    self.start()
+                    print("start",self.gear)
+                    some_key_pressed = True
+                    
+        if not some_key_pressed:
+            if self.flag%3 == 0:
+                if d == 0:
+                    self.wheel(w)
+            elif self.flag%3 == 1:
+                self.accelerator(a)
+            elif self.flag%3 == 2:
+                self.decelerator(d)
+            self.flag += 1
+
+        print(self.acc,self.dec,self.handle,self.gear)
+        self.update_window()
 ############################################################
 # 初期状態に戻す 計測開始
 # start 
@@ -108,6 +151,7 @@ class Car:
 
 # initialize
 car = Car()
+car.create_window()
 wait = 0.05
 car.connect(args.host,args.port)
 time.sleep(wait)
@@ -131,7 +175,7 @@ def fft_and_filter( data ):
     return result[idx], freq[idx]
 
 def accel_low_pitch( data, freq ):
-    return data * np.exp(-freq/30)
+    return data * freq**-2
 
 import matplotlib.pyplot as plt
 while input_stream.is_active():
@@ -139,6 +183,7 @@ while input_stream.is_active():
     input = np.frombuffer(input, dtype=np.int16)/max_val
     result,freq = fft_and_filter( input )
     low_pitch = accel_low_pitch(result,freq)
-    pitch = freq[np.argmax(result)]
+    max_idx = np.argmax(low_pitch)
+    pitch = freq[max_idx]
     volume = np.average(np.sort(result)[-1:])
     car.update( pitch, volume )
